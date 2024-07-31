8f6ac4331fae570e8b519580790fcfa00760e6d1
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-07-31 19:33:44 +0530

    created learn component

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,17 +1,53 @@
 import FeedWrapper from "@/components/feed-wrapper";
 import StickyWrapper from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+import { getUnits, getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+import { Unit } from "./unit";
 
-const LearnPage = () =>{
-    return(
-        <div className="flex flex-row-reverse gap-[48px] px-6">
-                <StickyWrapper>
-                    My Stricky warpper
-                </StickyWrapper>
-                <FeedWrapper>
-                    My Feed wrapper
-                </FeedWrapper>
-        </div>
-    )
-}
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+  const unitsData = getUnits();
+
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  return (
+    <div className='flex flex-row-reverse gap-[48px] px-6'>
+      <StickyWrapper>
+        <UserProgress
+          activeCource={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title={userProgress.activeCourse.title} />
+        {/* Render units */}
+        {units.map((unit) => (
+          <div key={unit.id} className='mb-10'>
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={undefined}
+              activeLessonPercentage={0}
+            />
+          </div>
+        ))}
+      </FeedWrapper>
+    </div>
+  );
+};
 
 export default LearnPage;

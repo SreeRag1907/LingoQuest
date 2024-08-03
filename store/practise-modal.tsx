bbde8b462bcf7eade422a11f8285d0433b730629
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-08-03 11:22:15 +0530

    created store for heart and practise session

---
+++ store/practise-modal.tsx
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+type PractiseModalState = {
+  isOpen: boolean;
+  open: () => void;
+  close: () => void;
+};
+
+export const UsePractiseModal = create<PractiseModalState>((set) => ({
+  isOpen: false,
+  open: () => set({ isOpen: true }),
+  close: () => set({ isOpen: false }),
+}));

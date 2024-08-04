2c157d4b74864687ce1ffca1f370ab21ee360dd5
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-08-04 16:02:48 +0530

    added user progress to mobile header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -13,6 +13,7 @@ import { redirect } from "next/navigation";
 import { Unit } from "./unit";
 import { Promo } from "@/components/promo";
 import { Quests } from "@/components/quests";
+import MobileHeader from "@/components/mobile-header";
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
@@ -42,35 +43,43 @@ const LearnPage = async () => {
   const isPro = !!userSubscription?.isActive;
 
   return (
-    <div className='flex flex-row-reverse gap-[48px] px-6'>
-      <StickyWrapper>
-        <UserProgress
-          activeCource={userProgress.activeCourse}
-          hearts={userProgress.hearts}
-          points={userProgress.points}
-          hasActiveSubscription={isPro}
-        />
-        {!isPro && <Promo />}
-        <Quests points={userProgress.points}/>
-      </StickyWrapper>
-      <FeedWrapper>
-        <Header title={userProgress.activeCourse.title} />
-        {/* Render units */}
-        {units.map((unit) => (
-          <div key={unit.id} className='mb-10'>
-            <Unit
-              id={unit.id}
-              order={unit.order}
-              description={unit.description}
-              title={unit.title}
-              lessons={unit.lessons}
-              activeLesson={courseProgress?.activeLesson}
-              activeLessonPercentage={lessonPercentage}
-            />
-          </div>
-        ))}
-      </FeedWrapper>
-    </div>
+    <>
+      <MobileHeader
+        activeCource={userProgress.activeCourse}
+        points={userProgress.points}
+        hearts={userProgress.hearts}
+        hasActiveSubscription={isPro}
+      />
+      <div className='flex flex-row-reverse gap-[48px] px-6'>
+        <StickyWrapper>
+          <UserProgress
+            activeCource={userProgress.activeCourse}
+            hearts={userProgress.hearts}
+            points={userProgress.points}
+            hasActiveSubscription={isPro}
+          />
+          {!isPro && <Promo />}
+          <Quests points={userProgress.points} />
+        </StickyWrapper>
+        <FeedWrapper>
+          <Header title={userProgress.activeCourse.title} />
+          {/* Render units */}
+          {units.map((unit) => (
+            <div key={unit.id} className='mb-10'>
+              <Unit
+                id={unit.id}
+                order={unit.order}
+                description={unit.description}
+                title={unit.title}
+                lessons={unit.lessons}
+                activeLesson={courseProgress?.activeLesson}
+                activeLessonPercentage={lessonPercentage}
+              />
+            </div>
+          ))}
+        </FeedWrapper>
+      </div>
+    </>
   );
 };
 
--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -1,9 +1,73 @@
+import React from 'react';
 import MobileSidebar from "./mobile-sidebar";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
+import { courses } from "@/db/schema";
 
-const MobileHeader = () => {
+type Props = {
+  activeCourse: typeof courses.$inferSelect | null;
+  points: number;
+  hearts: number;
+  hasActiveSubscription: boolean;
+};
+
+const MobileHeader: React.FC<Props> = ({
+  activeCourse,
+  points,
+  hearts,
+  hasActiveSubscription,
+}) => {
   return (
     <nav className='lg:hidden px-6 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50'>
-      <MobileSidebar/>
+      <MobileSidebar />
+      <div className='flex items-center justify-end gap-x-2 w-full'>
+        <Link href='/courses'>
+          <Button variant='ghost'>
+            {activeCourse && (
+              <Image
+                src={activeCourse.imageSrc}
+                alt={activeCourse.title}
+                width={50}
+                height={50}
+                className='rounded-md'
+              />
+            )}
+          </Button>
+        </Link>
+        <div className='bg-white rounded-xl'>
+
+        <Link href='/shop'>
+          <Button variant='ghost' className='text-orange-500'>
+            <Image
+              src='/points.svg'
+              alt='points'
+              width={28}
+              height={28}
+              className='mr-2'
+            />
+            {points}
+          </Button>
+        </Link>
+        <Link href='/shop'>
+          <Button variant='ghost' className='text-rose-500'>
+            <Image
+              src='/heart.svg'
+              alt='hearts'
+              width={28}
+              height={28}
+              className='mr-2'
+              />
+            {hasActiveSubscription ? (
+              <InfinityIcon className='stroke-[3]' />
+            ) : (
+              hearts
+            )}
+          </Button>
+        </Link>
+            </div>
+      </div>
     </nav>
   );
 };

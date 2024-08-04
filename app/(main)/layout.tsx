c160e56cf43747c9ede2de30789686b1086cea87
C.SREERAG <99243618+SreeRag1907@users.noreply.github.com>
2024-08-04 16:08:52 +0530

    changes in layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,15 +1,32 @@
+import React from 'react';
 import MobileHeader from "@/components/mobile-header";
 import { Sidebar } from "@/components/Sidebar";
+import { courses } from '@/db/schema';
 
 type Props = {
   children: React.ReactNode;
+  activeCourse: typeof courses.$inferSelect | null;
+  points: number;
+  hearts: number;
+  hasActiveSubscription: boolean;
 };
 
-const MainPage = ({ children }: Props) => {
+const MainPage = ({
+  children,
+  activeCourse,
+  points,
+  hearts,
+  hasActiveSubscription,
+}: Props) => {
   return (
     <div className='h-screen flex flex-col'>
       <div className='bg-green-100/70 flex-1'>
-        <MobileHeader />
+        <MobileHeader
+          activeCourse={activeCourse}
+          points={points}
+          hearts={hearts}
+          hasActiveSubscription={hasActiveSubscription}
+        />
         <Sidebar className='hidden lg:flex' />
         <main className='lg:pl-[256px] pt-[50px] lg:pt-0'>
           <div className='max-w-[1256px] bg-white mx-auto pt-6 h-full'>
